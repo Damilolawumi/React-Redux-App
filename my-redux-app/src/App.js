4daab2740cdb6a7dc2226b9ce5bb3bd82f683e16
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-10 19:16:13 +0100

    added a few changes

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Counter from '../src/components/Counter';
+import NasaImage from '../src/components/NasaImage';
+
 
 
 export default function App () {
 
   return(
-    <>
+    <div>
     <Counter/>
-    </>
+    <NasaImage/>
+    </div>
   )
 }
 
